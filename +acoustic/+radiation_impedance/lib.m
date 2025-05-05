a61bd7b90628492beb15bd583e51d176b54d9d84
jibrahim <jihad.ibrahim@mathworks.com>
2025-05-05 12:24:46 -0400

    Add files via upload

---
+++ +acoustic/+radiation_impedance/lib.m
@@ -0,0 +1,10 @@
+function lib( libInfo )
+% Customize library
+
+%   Copyright 2025 The MathWorks, Inc.
+
+libInfo.Name = 'Radiation Impedance';
+libInfo.Annotation = sprintf('This library contains radiation impedance elements');
+libInfo.ShowName = true;
+
+end
