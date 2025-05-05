76a627bcb3e9b9ccee72d3dc2a94ff5d9d408441
jibrahim <jihad.ibrahim@mathworks.com>
2025-05-05 12:24:11 -0400

    Add files via upload

---
+++ +acoustic/+elements/lib.m
@@ -0,0 +1,10 @@
+function lib( libInfo )
+% Customize library
+
+%   Copyright 2025 The MathWorks, Inc.
+
+libInfo.Name = 'Acoustical Elements';
+libInfo.Annotation = sprintf('This library contains basic acoustical elements');
+libInfo.ShowName = true;
+
+end
